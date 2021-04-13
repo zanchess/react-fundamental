df7b0c5e0ede0f221fcd63af41a51d310fea16e0
Aliaksandr Lashkevich <zanchesssss@gmail.com>
2021-04-13 14:09:41 +0300

    init: initial commit react-fundamental

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './app.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="content">
+          <h1>Hello world</h1>
+        </div>
+      </>
+    );
+  }
+}
+
+export default App;
