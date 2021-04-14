00568b1085bb9d016569dd9cae0acd31fd017e6b
Aliaksandr Lashkevich <zanchesssss@gmail.com>
2021-04-14 13:49:25 +0300

    feat: added form for login

---
+++ src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Form, Button } from 'react-bootstrap';
+import './login-form.scss';
+
+const LoginForm = () => (
+  <>
+    <Form className="login-form">
+      <Form.Group controlId="formBasicEmail">
+        <Form.Label>Login</Form.Label>
+        <Form.Control type="text" placeholder="Login" />
+      </Form.Group>
+
+      <Form.Group controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Sign in
+      </Button>
+    </Form>
+  </>
+);
+
+export default LoginForm;
--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
+import LoginForm from '../../components/LoginForm/LoginForm';
+import './LoginPage.scss';
 
 const LoginPage = () => (
-  <>
+  <div className="login-page">
     <Header />
-  </>
+    <LoginForm />
+  </div>
 );
 
 export default LoginPage;
