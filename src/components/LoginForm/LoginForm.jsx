c2445e6c0f796ecd56bdf14c6a83a37eb2456ca2
Aliaksandr Lashkevich <zanchesssss@gmail.com>
2021-04-19 22:22:04 +0300

    feat: added password validation messages

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -3,37 +3,35 @@ import { Form, Button, Row } from 'react-bootstrap';
 import './login-form.scss';
 
 const LoginForm = () => {
+  // reg exp for validation
   const loginRegExp = /^[A-z]+$/;
   const passwordRegExp = /^[A-z0-9]+$/;
 
+  // state for login input
   const [login, setLogin] = useState('');
   const [invalidLoginMessage, showInvalidLoginMessage] = useState(false);
   const [emptyLoginMessage, showEmptyLoginMessage] = useState(false);
 
+  // state for password input
   const [password, setPassword] = useState('');
   const [invalidPasswordMessage, showInvalidPasswordMessage] = useState(false);
-  const [passwordPlaceHolderValue, setPasswordPlaceHolderValue] = useState('password');
+  const [emptyPasswordMessage, showEmptyPasswordMessage] = useState(false);
 
+  // handler for change event
   const handleChange = (event) => {
     if (event.target.name === 'login') setLogin(event.target.value);
     if (event.target.name === 'password') setPassword(event.target.value);
   };
 
-  const passwordHandleChange = (event) => {
-    setPassword(event.target.value);
-  };
-
-  const loginHandleBlur = () => {
-    login === '' ? showEmptyLoginMessage(true) : showEmptyLoginMessage(false);
-    !loginRegExp.test(login) && login !== '' ? showInvalidLoginMessage(true) : showInvalidLoginMessage(false);
-  };
-
-  const passwordHandleBlur = () => {
-    if (password === '') {
-      console.log('enter password');
+  // handler for blur event
+  const handleBlur = (event) => {
+    if (event.target.name === 'login') {
+      login === '' ? showEmptyLoginMessage(true) : showEmptyLoginMessage(false);
+      !loginRegExp.test(login) && login !== '' ? showInvalidLoginMessage(true) : showInvalidLoginMessage(false);
     }
-    if (!passwordRegExp.test(password) && password !== '') {
-      console.log('incorrect password');
+    if (event.target.name === 'password') {
+      password === '' ? showEmptyPasswordMessage(true) : showEmptyPasswordMessage(false);
+      !passwordRegExp.test(password) && password !== '' ? showInvalidPasswordMessage(true) : showInvalidPasswordMessage(false);
     }
   };
 
@@ -43,14 +41,16 @@ const LoginForm = () => {
         <Form.Group controlId="for">
           <Row />
           <Form.Label>Login</Form.Label>
-          <Form.Control className={invalidLoginMessage || emptyLoginMessage ? 'login-incorrect-border' : ''} value={login} onBlur={loginHandleBlur} onChange={handleChange} name="login" type="text" placeholder="Login" />
+          <Form.Control className={invalidLoginMessage || emptyLoginMessage ? 'login-incorrect-border' : null} value={login} onBlur={handleBlur} onChange={handleChange} name="login" type="text" placeholder="Login" />
           <div className={invalidLoginMessage ? 'login-incorrect' : 'login-correct'}><span>Incorrect login</span></div>
           <div className={emptyLoginMessage ? 'login-empty' : 'login-not-empty'}><span>Please enter login</span></div>
         </Form.Group>
 
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control onBlur={passwordHandleBlur} onChange={handleChange} name="password" type="password" placeholder={passwordPlaceHolderValue} />
+          <Form.Control className={invalidPasswordMessage || emptyPasswordMessage ? 'login-incorrect-border' : null} onBlur={handleBlur} onChange={handleChange} name="password" type="password" placeholder="password" />
+          <div className={invalidPasswordMessage ? 'password-incorrect' : 'password-correct'}><span>Incorrect password</span></div>
+          <div className={emptyPasswordMessage ? 'password-empty' : 'password-not-empty'}><span>Please enter password</span></div>
         </Form.Group>
         <Button variant="primary" type="submit" disabled={!(login !== '' && password !== '')}>
           Sign in
