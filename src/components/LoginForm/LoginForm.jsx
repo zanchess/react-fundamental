c04e236e6fa720f5eb9a7e53bf9bc056c22e2305
Aliaksandr Lashkevich <zanchesssss@gmail.com>
2021-04-19 20:53:15 +0300

    feat: added messages for login validation

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Row } from 'react-bootstrap';
 import './login-form.scss';
 
 const LoginForm = () => {
@@ -7,7 +7,12 @@ const LoginForm = () => {
   const passwordRegExp = /^[A-z0-9]+$/;
 
   const [login, setLogin] = useState('');
+  const [invalidLoginMessage, showInvalidLoginMessage] = useState(false);
+  const [emptyLoginMessage, showEmptyLoginMessage] = useState(false);
+
   const [password, setPassword] = useState('');
+  const [invalidPasswordMessage, showInvalidPasswordMessage] = useState(false);
+  const [passwordPlaceHolderValue, setPasswordPlaceHolderValue] = useState('password');
 
   const handleChange = (event) => {
     if (event.target.name === 'login') setLogin(event.target.value);
@@ -19,12 +24,8 @@ const LoginForm = () => {
   };
 
   const loginHandleBlur = () => {
-    if (login === '') {
-      console.log('enter');
-    }
-    if (!loginRegExp.test(login) && login !== '') {
-      console.log('incorrect login');
-    }
+    login === '' ? showEmptyLoginMessage(true) : showEmptyLoginMessage(false);
+    !loginRegExp.test(login) && login !== '' ? showInvalidLoginMessage(true) : showInvalidLoginMessage(false);
   };
 
   const passwordHandleBlur = () => {
@@ -40,13 +41,16 @@ const LoginForm = () => {
     <>
       <Form className="login-form">
         <Form.Group controlId="for">
+          <Row />
           <Form.Label>Login</Form.Label>
-          <Form.Control value={login} onBlur={loginHandleBlur} onChange={handleChange} name="login" type="text" placeholder="Login" />
+          <Form.Control className={invalidLoginMessage || emptyLoginMessage ? 'login-incorrect-border' : ''} value={login} onBlur={loginHandleBlur} onChange={handleChange} name="login" type="text" placeholder="Login" />
+          <div className={invalidLoginMessage ? 'login-incorrect' : 'login-correct'}><span>Incorrect login</span></div>
+          <div className={emptyLoginMessage ? 'login-empty' : 'login-not-empty'}><span>Please enter login</span></div>
         </Form.Group>
 
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control onBlur={passwordHandleBlur} onChange={handleChange} name="password" type="password" placeholder="Password" />
+          <Form.Control onBlur={passwordHandleBlur} onChange={handleChange} name="password" type="password" placeholder={passwordPlaceHolderValue} />
         </Form.Group>
         <Button variant="primary" type="submit" disabled={!(login !== '' && password !== '')}>
           Sign in
