51deb06526bcabb704e6abce3505485bfb96eb63
Aliaksandr Lashkevich <zanchesssss@gmail.com>
2021-05-14 23:22:44 +0300

    feat: added redux and reducers

---
+++ src/store/authors/reducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  authors: [],
+};
+
+function authors(state = initialState, action) {
+  switch (action.type) {
+    default:
+      break;
+  }
+  return state;
+}
+
+export default authors;
---
+++ src/store/courses/reducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  courses: [],
+};
+
+function courses(state = initialState, action) {
+  switch (action.type) {
+    default:
+      break;
+  }
+  return state;
+}
+
+export default courses;
